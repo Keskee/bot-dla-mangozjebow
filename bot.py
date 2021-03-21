0eb3bf56fb1dec89d5d79eb026c7ac00dfa8e81b
Keskee <81095440+Keskee@users.noreply.github.com>
2021-03-21 19:13:51 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,5 +7,5 @@ while True:
   d = datetime.datetime.today()
   time.sleep(3)
   if d.hour == 20 and d.minute == 37:
-       hook.send("<@&822960885156806697> ")
+       hook.send("<@&822960885156806697>")
        time.sleep(60)
