4bfaab316c6f9f6893aaf504e2c6d8fe1a84058f
Keskee <81095440+Keskee@users.noreply.github.com>
2021-03-21 21:42:24 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-from dhooks import Webhook
+from dhooks import Webhook, File
 import datetime
 import time
 hook = Webhook('https://discord.com/api/webhooks/822960833339588608/jR4wJDqZMqvINZt2RjhZ8x8ed3_XqYgfX_fE9ydAHbV-0X0eT0u2qQV_UvEE9GC6zHTl')
