8e01f79ac51c807a229b9f3b608bb6a88875da8e
Keskee <81095440+Keskee@users.noreply.github.com>
2021-03-22 23:00:21 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,11 +2,11 @@ from dhooks import Webhook, File
 import datetime
 import time
 hook = Webhook('https://discord.com/api/webhooks/822960791849664562/mjYqmK9LLYGECgASlw84kw-VTz1L0wcw_AnappyCEQjojp1eourOQvi0cTQRFeF4DowO')
-zdj = File("gif.gif")
+zdj = File("marysia.png")
 d = datetime.datetime.today()
 while True:
   d = datetime.datetime.today()
   time.sleep(3)
-  if d.hour == 21 and d.minute == 59:
-       hook.send("Test gifa", file = zdj)
+  if d.hour == 20 and d.minute == 37:
+       hook.send("<@&822960885156806697> Czas na joge z marysia!", file = zdj)
        time.sleep(60)
