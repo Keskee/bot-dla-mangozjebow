c3e908cac1985fcb7857484bfa2522d336a861cd
Keskee <81095440+Keskee@users.noreply.github.com>
2021-03-22 23:21:52 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,6 +7,6 @@ d = datetime.datetime.today()
 while True:
   d = datetime.datetime.today()
   time.sleep(3)
-  if d.hour == 22 and d.minute == 25:
-       hook.send("Test", file = zdj)
+  if d.hour == 22 and d.minute == 28:
+       hook.send('Test', file = zdj)
        time.sleep(60)
