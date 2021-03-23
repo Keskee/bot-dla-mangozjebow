0514c83c1824af389f9470a31cd7e129d8cbd68b
Keskee <81095440+Keskee@users.noreply.github.com>
2021-03-23 09:02:04 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,11 +2,11 @@ from dhooks import Webhook, File
 import datetime
 import time
 hook = Webhook('https://discord.com/api/webhooks/822960791849664562/mjYqmK9LLYGECgASlw84kw-VTz1L0wcw_AnappyCEQjojp1eourOQvi0cTQRFeF4DowO')
-zdj = File("marysia.png")
+zdj = File("pikachu.gif")
 d = datetime.datetime.today()
 while True:
   d = datetime.datetime.today()
   time.sleep(3)
-  if d.hour == 22 and d.minute == 28:
-       hook.send('Test', file = zdj)
+  if d.hour == 8 and d.minute == 10:
+       hook.send('teścik', file = zdj)
        time.sleep(60)
