c3fce1640c6016a3625986f7361e3d1509fd7337
Keskee <81095440+Keskee@users.noreply.github.com>
2021-03-23 17:47:03 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,12 @@
 from dhooks import Webhook, File
 import datetime
 import time
-hook = Webhook('https://discord.com/api/webhooks/822960791849664562/mjYqmK9LLYGECgASlw84kw-VTz1L0wcw_AnappyCEQjojp1eourOQvi0cTQRFeF4DowO')
+hook = Webhook('https://discord.com/api/webhooks/822960833339588608/jR4wJDqZMqvINZt2RjhZ8x8ed3_XqYgfX_fE9ydAHbV-0X0eT0u2qQV_UvEE9GC6zHTl')
 zdj = File('pope.gif')
 d = datetime.datetime.today()
 while True:
   d = datetime.datetime.today()
   time.sleep(3)
-  if d.hour == 20 and d.minute == 37:
-       hook.send('<@&822960885156806697> Kręcimy śmigłem!', file = zdj)
+  if d.hour == 16 and d.minute == 52:
+       hook.send('<@&662300886671818752> Kręcimy śmigłem!', file = zdj)
        time.sleep(60)
