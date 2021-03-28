5551842e5102c9fc35c6198a377de5b26ec9d5ed
Keskee <81095440+Keskee@users.noreply.github.com>
2021-03-28 13:19:06 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,11 +2,11 @@ from dhooks import Webhook, File
 import datetime
 import time
 hook = Webhook('https://discord.com/api/webhooks/822960791849664562/mjYqmK9LLYGECgASlw84kw-VTz1L0wcw_AnappyCEQjojp1eourOQvi0cTQRFeF4DowO')
-zdj = File('pikachu.gif')
+zdj = File('animepapiez.gif')
 d = datetime.datetime.today()
 while True:
   d = datetime.datetime.today()
   time.sleep(3)
-  if d.hour == 11 and d.minute == 10:
-       hook.send('test, bo zmiana czasu', file = zdj)
+  if d.hour == 19 and d.minute == 37:
+       hook.send('<@&822960885156806697> ¯\(°_o)/¯', file = zdj)
        time.sleep(60)
