31c149577c4347618781c13ad9c61a1b978e93f5
Keskee <81095440+Keskee@users.noreply.github.com>
2021-03-29 10:33:04 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,11 +2,11 @@ from dhooks import Webhook, File
 import datetime
 import time
 hook = Webhook('https://discord.com/api/webhooks/822960791849664562/mjYqmK9LLYGECgASlw84kw-VTz1L0wcw_AnappyCEQjojp1eourOQvi0cTQRFeF4DowO')
-zdj = File('animepapiez.gif')
+zdj = File('pope.gif')
 d = datetime.datetime.today()
 while True:
   d = datetime.datetime.today()
   time.sleep(3)
   if d.hour == 19 and d.minute == 37:
-       hook.send('<@&822960885156806697> ¯\(°_o)/¯', file = zdj)
+       hook.send('<@&822960885156806697> :PepeAhegao:', file = zdj)
        time.sleep(60)
