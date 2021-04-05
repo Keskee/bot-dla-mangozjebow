5a7155b1ec7baf1787a23406b400c7321d9b201b
Keskee <81095440+Keskee@users.noreply.github.com>
2021-04-05 18:25:58 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,10 +3,10 @@ import datetime
 import time
 hook = Webhook('https://discord.com/api/webhooks/822960791849664562/mjYqmK9LLYGECgASlw84kw-VTz1L0wcw_AnappyCEQjojp1eourOQvi0cTQRFeF4DowO')
 d = datetime.datetime.today()
-zdj = File('wielkanoc.jpg')
+zdj = File('kremowki.jpg')
 while True:
   d = datetime.datetime.today()
   time.sleep(3)
   if d.hour == 19 and d.minute == 37:
-       hook.send('<@&822960885156806697> Wszystkiego najlepszego od Jana Pawła II Wielkiego!', file = zdj)
+       hook.send('<@&822960885156806697> lanego Poniedziałku czy coś', file = zdj)
        time.sleep(60)
