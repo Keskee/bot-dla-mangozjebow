57442533260a5da73725d29210901ce4bd6a1817
Keskee <81095440+Keskee@users.noreply.github.com>
2021-04-06 12:10:48 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,12 @@
 from dhooks import Webhook, File
 import datetime
 import time
-hook = Webhook('https://discord.com/api/webhooks/822960791849664562/mjYqmK9LLYGECgASlw84kw-VTz1L0wcw_AnappyCEQjojp1eourOQvi0cTQRFeF4DowO')
+hook = Webhook('https://discord.com/api/webhooks/822960833339588608/jR4wJDqZMqvINZt2RjhZ8x8ed3_XqYgfX_fE9ydAHbV-0X0eT0u2qQV_UvEE9GC6zHTl')
 d = datetime.datetime.today()
 zdj = File('kremowki.jpg')
 while True:
   d = datetime.datetime.today()
   time.sleep(3)
-  if d.hour == 19 and d.minute == 37:
-       hook.send('<@&822960885156806697> lanego Poniedziałku czy coś', file = zdj)
+  if d.hour == 10 and d.minute == 20:
+       hook.send('<@&662300886671818752>', file = zdj)
        time.sleep(60)
